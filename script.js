f6f3015ebfd6a7aa81c163ad2cab8b6d3b3a4e15
giftmshengu250-pixel <giftmshengu250@gmail.com>
2026-05-08 16:29:18 +0200

    list-items

---
+++ script.js
@@ -0,0 +1,14 @@
+// Data to search from
+
+const items = [
+  "HTML Tutorial",
+  "CSS Responsive Design",
+  "JavaScript Basics",
+  "Frontend Projects",
+  "React Tutorial",
+  "Live Search Bar",
+  "Web Development",
+  "Responsive Navbar",
+  "Portfolio Website",
+  "CSS Animations"
+];
