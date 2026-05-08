20ecc7722e675075c0127416be4b8e085ab55aea
Dido <masengodido28@gmail.com>
2026-05-08 16:52:03 +0200

    filter item

--- script.js
+++ script.js
@@ -1,7 +1,81 @@
+// Data to search from
+
+const items = [
+  "HTML Tutorial",
+  "CSS Responsive Design",
+  "JavaScript Basics",
+  "Frontend Projects",
+  "React Tutorial",
+  "Live Search Bar",
+  "Web Development",
+  "Responsive Navbar",
+  "Portfolio Website",
+  "CSS Animations"
+];
 
 
-function filterItems(input) {
-return items.filter(item =>
-item.toLowerCase().includes(input.toLowerCase())
-);
-}
+// Get HTML elements
+
+const searchInput = document.getElementById("searchInput");
+const results = document.getElementById("results");
+
+
+// Listen for typing
+
+searchInput.addEventListener("keyup", () => {
+
+  // Get what user typed
+  const input = searchInput.value.toLowerCase();
+
+  // Clear old results
+  results.innerHTML = "";
+
+  // If input is empty
+  if(input === ""){
+    results.style.display = "none";
+    return;
+  }
+
+  // Filter matching items
+
+  const filteredItems = items.filter(item =>
+    item.toLowerCase().includes(input)
+  );
+
+  // Show results box
+  results.style.display = "block";
+
+  // If no match found
+
+  if(filteredItems.length === 0){
+
+    results.innerHTML = `
+      <div class="no-result">
+        No results found
+      </div>
+    `;
+  }
+
+  // Show matching items
+
+  filteredItems.forEach(item => {
+
+    const li = document.createElement("li");
+
+    li.textContent = item;
+
+    // Click result to fill input
+
+    li.addEventListener("click", () => {
+
+      searchInput.value = item;
+
+      results.style.display = "none";
+
+    });
+
+    results.appendChild(li);
+
+  });
+
+});
